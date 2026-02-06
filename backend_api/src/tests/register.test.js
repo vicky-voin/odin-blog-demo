f30f26bc052d6fe405f6ab99f1ee96f58295afb3
vicky-voin <alteneria127@gmail.com>
2026-02-06 12:42:42 -0500

    Added coverage for user data validation

--- backend_api/src/tests/register.test.js
+++ backend_api/src/tests/register.test.js
@@ -20,6 +20,8 @@ app.use("/register", registerRouter);
 
 setupErrorHandling(app);
 
+const VERY_LONG_STRING = "t".repeat(256);
+
 describe("POST /register", function () {
   it("receives incorrect properties, responds with 400 error", (done) => {
     request(app)
@@ -29,19 +31,74 @@ describe("POST /register", function () {
       .expect("Content-type", /json/)
       .expect(400, done);
   });
-  it("receives an invalid username, responds with 400 error", (done) => {
-    request(app)
-      .post("/register")
-      .type("form")
-      .send({
-        username: "not a proper email",
-        first_name: process.env.TEST_USER_FIRST_NAME,
-        last_name: process.env.TEST_USER_LAST_NAME,
-        password: process.env.TEST_USER_PW,
-      })
-      .expect("Content-type", /json/)
-      .expect(400, done);
-  });
+  it.each(["", "123", "test@test"])(
+    "receives an invalid username (%s), responds with 400 error",
+    async (invalidValue) => {
+      request(app)
+        .post("/register")
+        .type("form")
+        .send({
+          username: invalidValue,
+          first_name: process.env.TEST_USER_FIRST_NAME,
+          last_name: process.env.TEST_USER_LAST_NAME,
+          password: process.env.TEST_USER_PW,
+        })
+        .expect("Content-type", /json/)
+        .expect(400)
+        .end();
+    },
+  );
+  it.each(["", "123", "pw123", "Pw1"])(
+    "receives an invalid password (%s), responds with 400 error",
+    async (invalidValue) => {
+      request(app)
+        .post("/register")
+        .type("form")
+        .send({
+          username: process.env.TEST_USER_NAME,
+          first_name: process.env.TEST_USER_FIRST_NAME,
+          last_name: process.env.TEST_USER_LAST_NAME,
+          password: invalidValue,
+        })
+        .expect("Content-type", /json/)
+        .expect(400)
+        .end();
+    },
+  );
+  it.each(["", "123", "myname.", VERY_LONG_STRING])(
+    "receives an invalid first name (%s), responds with 400 error",
+    async (invalidValue) => {
+      request(app)
+        .post("/register")
+        .type("form")
+        .send({
+          username: process.env.TEST_USER_NAME,
+          first_name: invalidValue,
+          last_name: process.env.TEST_USER_LAST_NAME,
+          password: process.env.TEST_USER_PW,
+        })
+        .expect("Content-type", /json/)
+        .expect(400)
+        .end();
+    },
+  );
+  it.each(["", "123", "myname.", VERY_LONG_STRING])(
+    "receives an invalid last name (%s), responds with 400 error",
+    async (invalidValue) => {
+      request(app)
+        .post("/register")
+        .type("form")
+        .send({
+          username: process.env.TEST_USER_NAME,
+          first_name: process.env.TEST_USER_FIRST_NAME,
+          last_name: invalidValue,
+          password: process.env.TEST_USER_PW,
+        })
+        .expect("Content-type", /json/)
+        .expect(400)
+        .end();
+    },
+  );
   it("receives acceptable data, responds with success", (done) => {
     const userData = {
       username: process.env.TEST_USER_NAME,
